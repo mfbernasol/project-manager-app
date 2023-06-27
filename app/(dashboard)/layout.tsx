0f2b4fa0af054bf2a6207ac97fc6508e1b327b6b
mfbernasol <mfbernasol@gmail.com>
2023-06-26 20:57:01 -0500

    create greetings component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,20 +1,28 @@
 import "@/styles/global.css";
-
+import { Inter } from "@next/font/google";
 import GlassPane from "@/components/GlassPane";
+import Sidebar from "@/components/Sidebar";
 import { ReactElement } from "react";
 
-type DashboardRootLayout = {
+
+type DashboardRootLayoutProps = {
   children: ReactElement;
 };
 
-export default function DashboardRootLayout({ children }: DashboardRootLayout) {
+const inter = Inter({
+  variable: "--font-inter",
+});
+
+export default function DashboardRootLayout({ children }: DashboardRootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <head />
-      <body className="h-screen w-screen rainbow-mesh p-6">
-        <GlassPane className="w-full h-full flex items-center justify-center">
+      <body className="h-screen w-screen candy-mesh p-6">
+        <GlassPane className="w-full h-full flex items-center gap-2">
+          <Sidebar/>
           {children}
         </GlassPane>
+        <div id="modal"></div>
       </body>
     </html>
   );
