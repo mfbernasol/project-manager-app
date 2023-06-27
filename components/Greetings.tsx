9c23ba06ee7deccd7b0bfa9c0bf5435a51a6920f
mfbernasol <mfbernasol@gmail.com>
2023-06-26 21:31:52 -0500

    create project Card component

--- components/Greetings.tsx
+++ components/Greetings.tsx
@@ -28,7 +28,7 @@ const Greetings = async () => {
         </h4>
       </div>
       <div>
-        <Button size="large">Today&aposs Schedule</Button>
+        <Button size="large">Today&apos;s Schedule</Button>
       </div>
     </Card>
   );
